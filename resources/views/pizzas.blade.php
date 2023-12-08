7f1c12fcaee6355e1777297f4dabf09639546d2f
Matthew Fekumoh <mmattez2009@gmail.com>
2023-12-08 17:53:38 +0100

    edited the pizza template

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,4 +1,6 @@
+    @extends('Layouts.layout')
 
+    @section('content')
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -19,10 +21,6 @@
                     Pizza List 
                 </div>
 
-                    <!-- @for($i = 0; $i < count($pizzas); $i++)
-                        <p>{{ $pizzas[$i]['type'] }}</p>
-                    @endfor -->
-
                     @foreach($pizzas as $pizza)
                         <div>
                           {{$loop->index}}  {{$pizza['type']}} - {{ $pizza['base']}}
@@ -35,17 +33,7 @@
                         </div>
                     @endforeach
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
-    </body>
-</html>
+    @endsection
+
