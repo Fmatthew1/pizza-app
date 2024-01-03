cec51222271b79da44c843ee14edf4a79518a9f3
Matthew Fekumoh <mmattez2009@gmail.com>
2024-01-03 17:19:20 +0100

    updated the sass

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -1,35 +1,37 @@
 @extends('Layouts.layout')
 
 @section('content')
-
-<table class="table display compact data-table">
-                            <thead>
-                                @foreach($services as $service)
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Price</th>
-                                    <th>Description</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Diesel</td>
-                                    <td>11000</td>
-                                    <td>Buy from recognized dealer</td>
-                                </tr>
-                                <tr>
-                                    <td>Machine Parts</td>
-                                    <td>3000</td>
-                                    <td>Buy the original parts</td>
-                                </tr>
-                                <tr>
-                                    <td>Auto-mobile</td>
-                                    <td>44000</td>
-                                    <td>purchase the original brand</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+<div class="">
+    <table class="wrapper">
+            <thead>
+                @foreach($services as $service)
+                <tr class="pizza-details">
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="pizza-details">
+                    <td>Diesel</td>
+                    <td>11000</td>
+                    <td>Buy from recognized dealer</td>
+                </tr>
+                <tr class="pizza-details">
+                    <td>Machine Parts</td>
+                    <td>3000</td>
+                    <td>Buy the original parts</td>
+                </tr>
+                <tr class="pizza-details">
+                    <td>Auto-mobile</td>
+                    <td>44000</td>
+                    <td>purchase the original brand</td>
+                </tr>
+                @endforeach
+            </tbody>
+    </table>
+</div>
+    
 
 
 @endsection
