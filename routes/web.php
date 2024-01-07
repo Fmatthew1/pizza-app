38c96260148cb4f366a4395b669757bd7eb0f2c7
Matthew Fekumoh <mmattez2009@gmail.com>
2024-01-08 00:39:53 +0100

    Added middleware to pizza route for Auth

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['namespace' => 'Product', 'prefix' => 'pizzas'], function(){
-    Route::get('', 'PizzaController@index');
-    Route::get('/create', 'PizzaController@create');
-    Route::post('', 'PizzaController@store');
-    Route::get('/{id}', 'PizzaController@show');
-    Route::delete('/{id}', 'PizzaController@destroy');
+    Route::get('', 'PizzaController@index')->name('pizzas.index')->middleware('auth');
+    Route::get('/create', 'PizzaController@create')->name('pizzas.create');
+    Route::post('', 'PizzaController@store')->name('pizzas.store');
+    Route::get('/{id}', 'PizzaController@show')->name('pizzas.show')->middleware('auth');
+    Route::delete('/{id}', 'PizzaController@destroy')->name('pizzas.destroy')->middleware('auth');
 });
 
 Route::get('/pizzass', 'Product\PizzaController@index');
 
-Route::get('/services', 'ServiceController@index')->name('index');
-Route::get('/services/create', 'ServiceController@create')->name('create');
-Route::post('services/store', 'ServiceController@store')->name('store');
-Route::get('/services/{id}', 'ServiceController@show')->name('show');
-Route::get('/services/{id}/edit', 'ServiceController@edit')->name('edit');
-Route::put('/services/{id}', 'ServiceController@update')->name('update');
-Route::delete('/services/{id}', 'ServiceController@destroy')->name('destroy');
+Route::get('/services', 'ServiceController@index')->name('services.index');
+Route::get('/services/create', 'ServiceController@create')->name('services.create');
+Route::post('services', 'ServiceController@store')->name('services.store');
+Route::get('/services/{id}', 'ServiceController@show')->name('services.show');
+Route::get('/services/{id}/edit', 'ServiceController@edit')->name('services.edit');
+Route::put('/services/{id}', 'ServiceController@update')->name('services.update');
+Route::delete('/services/{id}', 'ServiceController@destroy')->name('services.destroy');
 
 
 
     
+Auth::routes([
+    'register' => false
+]);
+
+Route::get('/home', 'HomeController@index')->name('home');
