ffb7d7dd459e41af2ac123dd20631fd963af4efe
Matthew Fekumoh <mmattez2009@gmail.com>
2024-01-08 00:32:43 +0100

    added styling

--- resources/views/Layouts/layout.blade.php
+++ resources/views/Layouts/layout.blade.php
@@ -6,6 +6,9 @@
 
         <title>Pizzahouse</title>
 
+        <!--Bootstrap--> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
