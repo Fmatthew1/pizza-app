3d096e21c557f6ab79405e979a9dd3723127b80c
Matthew Fekumoh <mmattez2009@gmail.com>
2024-01-11 15:06:18 +0100

    added error message

--- resources/views/services/update.blade.php
+++ resources/views/services/update.blade.php
@@ -2,12 +2,31 @@
 
 @section('content')
             <h1>Update Services</h1>
+            
         <form action="/services/{{ $service->id }}" method="post">
             @csrf
             @method('put')
-            <label for="description">Update Services</label>
-            <input type="text" id="description" name="description" value="{{ $service->description }}">
-            <button type="submit">Save</button>
+            <form>
+            <div>
+                <label for="description">Update Services</label>
+            </div>
+            
+                <input type="text" id="description" name="description" value="{{ $service->description }}">
+        
+            <button type="submit" class="btn btn-success">Update</button>
+            <div >
+                <a href="/services" class="back"><button class="btn btn-primary me-md-2" type="button">Go to Services</button></a>
+            </div>
+            </form>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                
+                    @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+            
+            </div>
+            @endif
         </form>
 
 @endsection
