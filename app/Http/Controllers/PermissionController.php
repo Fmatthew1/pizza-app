56bdd3729e305b056d82a8f327775cf51d6d9256
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-03 22:54:42 +0100

    configured the permission controller

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Permission;
+use App\Role;
 use Illuminate\Http\Request;
 
 
@@ -13,6 +14,62 @@ class PermissionController extends Controller
         return view('permissions.index',[
             'permissions' => $permissions,
         ]);
-        
     }
+
+    public function create()
+    {
+        
+        $permissions = new Permission();
+       
+        $permissions->name = request('name');
+        $permissions->description = request('description');
+        $permissions->permission_id = request('permission_id');
+        //dd($permissions);
+        return view('permissions.create', ['permissions'=> $permissions]);
+    }
+
+    public function store(Request $request)
+    {
+       
+        $request->validate([
+            'name'=> 'required|unique:permission|max:255',
+            'description'=> 'required',
+        ]); 
+        
+        $permission = new Permission();
+        $permission->name = $request->name;
+        $permission->description = $request->description;
+        $permission->save();
+
+        return redirect('permissions')->with('success','Permission successfully created');
+    }
+
+    public function edit($id)
+    {
+        $permission = Permission::findOrFail($id);
+        $permissions = Permission::all();
+        return view('permissions.update', ['permission' => $permission]);
+    }
+
+    public function show($id)
+    {
+        $permission = Permission::findOrFail($id);
+        return view('permissions.show', ['permission'=> $permission]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'=> 'required|unique:roles|max:255',
+            'description'=> 'required',
+        ]);
+        
+        $permission = Permission::findOrFail($id);
+        $permission->name = $request->name;
+        $permission->save();
+        return redirect('permissions');
+    
+    }
+
+        
 }
