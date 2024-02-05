cf15c6bc4efafdd9c0b9d8669d36baf64b07aa3e
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-05 13:45:33 +0100

    updated permissions table

--- database/migrations/2024_01_16_194131_create_permissions_table.php
+++ database/migrations/2024_01_16_194131_create_permissions_table.php
@@ -16,7 +16,7 @@ class CreatePermissionsTable extends Migration
         Schema::create('permissions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name')->unique();
-            $table->longText('description')->nullable();
+            $table->string('description');
 
             $table->timestamps();
 
