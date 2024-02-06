6a0c508a9c2c5c2b7c406fd2b77025a0c758f678
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-06 09:02:03 +0100

    Added the error message

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -3,7 +3,15 @@
 @section('content')
     <div class="container">
         <h2>Permission Management</h2>
-     
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
