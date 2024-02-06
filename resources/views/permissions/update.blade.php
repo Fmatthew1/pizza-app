566ab9c2bd1087097a85e9d5956f796ed5a8574a
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-06 08:18:51 +0100

    edited the update template

--- resources/views/permissions/update.blade.php
+++ resources/views/permissions/update.blade.php
@@ -36,25 +36,6 @@
 
                             </div>
 
-                            <div class="form-group row">
-                                <label class="col-md-4 col-form-label text-md-right" for="role_permission">Permissions</label>
-                                <div class="col-md-6">           
-                                    <select class="form-select @error('permissions') is-invalid @enderror" multiple aria-label="Permissions" id="permissions" name="permissions[]" style="height: 210px;">
-                                        @forelse ($permissions as $permission)
-                                            <option value="{{ $permission->id }}" {{ in_array($permission->id, $rolePermissions ?? []) ? 'selected' : '' }}>
-                                                {{ $permission->name }}
-                                            </option>
-                                        @empty
-        
-                                        @endforelse
-                                    </select>
-                                    @if ($errors->has('permissions'))
-                                        <span class="text-danger">{{ $errors->first('permissions') }}</span>
-                                    @endif
-                                </div>
-        
-                            </div>
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
