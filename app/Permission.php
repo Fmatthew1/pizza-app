a9cb47d0c2e9b2e8646fe51c532a6cdefafbaa33
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-09 09:30:17 +0100

    updated the model

--- app/Permission.php
+++ app/Permission.php
@@ -7,21 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-
-    protected $fillable = [
-        'name',
-        'description',
-    ];
-
     public function roles()
     {
         return $this->belongsToMany('App\Role');
     }
-    public function hasPermission($permission)
-    {
-        return $this->role->permissions()->where('name', 'description', $permission)->exists();
-    }
-
-
 }
 
