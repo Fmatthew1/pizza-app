184a63e40c65704eed25ab40ab8e4fe1e413b101
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-20 16:29:52 +0100

    little update

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,6 +5,9 @@
    <div class="wrapper-pizza-detals">
       <h1>Show A Single User</h1>
       <p>{{ $user->name }}</p>
+      <p>{{ $user->email }}</p>
+      <p>{{ $user->role_id }}</p>
+      
       <a href="/users" class="back">Back To All Users</a>
    </div>
 
