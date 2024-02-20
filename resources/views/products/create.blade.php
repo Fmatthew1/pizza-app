cbb1c9ef764d4cad6b8528c695ba14e753777cb1
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-20 16:29:34 +0100

    slight update

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,8 +1,9 @@
 @extends('Layouts.app')
 
 @section('content')
-    <h1>Create New product</h1>
-    @if ($errors->any())
+    <div class="container">
+        <h2>Create Products</h2>
+        @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
                     @foreach ($errors->all() as $error)
@@ -11,29 +12,43 @@
                 </ul>
             </div>
         @endif
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <form action="{{ route('products.store') }}" method="POST">
-                    @csrf
-                        <div class="mb-2">
-                        <label for="name" class="form-label">Product Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Product Name" value= "{{old('name')}}">
-                        </div>
-                        <div class="mb-2">
-                        <label for="price" class="form-label">Price</label>
-                        <input type="number" class="form-control" id="price" name="price" placeholder="Price" value= "{{old('price')}}">
-                        </div>
-                        <div class="mb-2">
-                        <label for="description" class="form-label">Description</label>
-                        <input type="text" class="form-control" id="description" name="description" placeholder="Description" value= "{{old('description')}}">
-                        </div>
-                        <div class="d-grid gap-2">
-                        <button class="btn btn-primary" type="submit" value="submit">Submit</button>
-                        </div>
-                    </form>
+        
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create New Product</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('products.store') }}">
+                        @csrf
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Product Name</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ 'name' }}">
+                                </div>
+                                <label for="price" class="col-md-4 col-form-label text-md-right">Price</label>
+                                <div class="col-md-6">
+                                    <input id="price" type="number" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ 'price' }}">
+                                </div>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Description</label>
+                                <div class="col-md-6">
+                                    <input id="decription" type="text" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ 'description' }}">
+                                </div>
+
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-   
 @endsection
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,49 +1,73 @@
 @extends('Layouts.app')
 
 @section('content')
-    <h1 class="text-align-center">Create New User</h1>
-    @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <div class="float-start">
+                    Create New User
+                </div>
+                
             </div>
-        @endif
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <form action="{{ route('users.store') }}" method="POST">
-                @csrf
-                    <div class="mb-2">
-                    <label for="name" class="form-label">New User</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder="User Name" value= "{{old('name')}}">
-                    </div><div class="mb-2">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Email" value= "{{old('email')}}">
-                    </div> 
-                    <div class="mb-2">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                    </div>
-                    <div class="mb-2">
-                    <label for="password_confirmation" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password">
-                    </div>
-                    <div class="d-grid gap-2">
+            <div class="card-body">
+                <form action="{{ route('users.store') }}" method="post">
+                    @csrf
 
-                        <h3>Select role</h3>
-                        <select id='role_id' name="role_id" required>
-                            @foreach($roles as $role)
-                                <option value="{{ $role->id }}">{{ $role->name }}</option>
-                            @endforeach
-                        </select>
-
-                    <button class="btn btn-primary" type="submit" value="submit">Submit</button>
+                    <div class="mb-3 row">
+                        <label for="name" class="col-md-4 col-form-label text-md-end text-start">New User</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value= "{{old('name')}}" placeholder="Enter your Username">
+                            @if ($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
                     </div>
+
+                    <div class="mb-3 row">
+                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email</label>
+                        <div class="col-md-6">
+                          <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value= "{{old('email')}}" placeholder="Enter your Email">
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
+                        <div class="col-md-6">
+                          <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password_confirmation" class="col-md-4 col-form-label text-md-end text-start">Confirm Password</label>
+                        <div class="col-md-6">
+                          <input type="password" class="form-control @error('password') is-invalid @enderror" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <label for="roles" class="col-md-4 col-form-label text-md-end text-start">Roles</label>
+                    <h4>Select role</h4>
+                    <select id='role_id' name="role_id" required>
+                        @foreach($roles as $role)
+                            <option value="{{ $role->id }}">{{ $role->name }}</option>
+                        @endforeach
+                    </select>
+                    
+                    <div class="mb-3 row">
+                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Create User">
+                    </div>
+                    
                 </form>
             </div>
         </div>
     </div>
+</div>    
 @endsection
