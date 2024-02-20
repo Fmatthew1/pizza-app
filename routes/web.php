461daed7fa5781fb190cc4aa6427f76f3a7ab840
Matthew Fekumoh <mmattez2009@gmail.com>
2024-02-20 16:33:42 +0100

    little update

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,7 @@ Route::put('/products/{id}', 'ProductController@update')->name('products.update'
 
     
 Auth::routes([
-    'register' => true
+    'register' => false
 ]);
 
 Route::get('/home', 'HomeController@index')->name('home');
